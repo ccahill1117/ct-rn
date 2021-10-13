00f2888bd0d59ced2c7be76966197821ab16e6db
Christopher Cahill <christopher.cahill@evanta.com>
2021-10-13 00:23:41 -0700

    basic nav stuff

---
+++ screens/DetailsScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native'
+
+const DetailsScreen = (props) => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        HOME SCREEN
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+
+export default DetailsScreen
---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+} from 'react-native'
+
+const HomeScreen = (props) => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        HOME SCREEN
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+
+
+export default HomeScreen
